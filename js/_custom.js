611881714dbfd51ff3ade4791068e098bc84296a
sirnet <artemshomin@gmail.com>
2019-11-21 20:34:28 +0300

    Add files via upload

---
+++ js/_custom.js
@@ -0,0 +1,22 @@
+    var burgerList = document.getElementById("burger__List");
+    var firstItem = document.getElementById("first");
+    var secondItem = document.getElementById("second");
+    var thirdItem = document.getElementById("third");
+    var fourthItem = document.getElementById("fourth");
+    var i=0;
+    burgerList.onclick =  function(){
+        if (i ==0) {
+            firstItem.classList.add("visually-hidden");
+            secondItem.classList.add("second--active");
+            thirdItem.classList.add("third--active");
+            fourthItem.classList.add("visually-hidden");
+            i=1;
+        }
+        else {
+            firstItem.classList.remove("visually-hidden");
+            secondItem.classList.remove("second--active");
+            thirdItem.classList.remove("third--active");
+            fourthItem.classList.remove("visually-hidden");
+            i=0;
+        }
+    } 
